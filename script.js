e830ef6fde0ffd092cc9a7eab580a067a599502c
Felipe dos Santos Bento <dsbfelipe@outlook.com>
2024-09-17 02:41:05 -0300

    Create all logic for the funcionality of the program

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-
-function createGrid() {
-  for (let i = 0; i < 16; i++) {
+function createGrid(numberOfSquares) {
+  for (let i = 0; i < numberOfSquares; i++) {
     const square = document.createElement("div");
     square.className = "square";
     container.appendChild(square);
   }
 }
 
-createGrid();
+function getSquares() {
+  const squareNodeList = document.querySelectorAll(".square");
+  squareNodeList.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.classList.add("paint");
+    })
+  })
+}
+
+button.addEventListener("click", () => {
+  let height = window.prompt("Set the height in squares");
+  if (height > 100) {
+    alert("Please insert a value lower or equal to 100");
+    height = window.prompt("Set the height in squares");
+  }
+  console.log(height);
+  const base = window.prompt("Set the width in squares");
+  if (base > 100) {
+    alert("Please insert a value lower or equal to 100");
+    base = window.prompt("Set the height in squares");
+  }
+  console.log(base);
+  clearContainer();
+  squareQuantity = setGridSize(base, height);
+  console.log(squareQuantity)
+  createGrid(squareQuantity);
+  getSquares();
+})
+
+function setGridSize(base, height) {
+  container.style.height = `${height * 10}px`;
+  container.style.width = `${base * 10}px`;
+  squareQuantity = base * height;
+  return squareQuantity;
+}
+
+function clearContainer() {
+  container.innerHTML = "";
+}
